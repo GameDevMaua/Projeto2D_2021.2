63e2d66f9c176a7187fa254aca6fadd243becac4
GuiBanzato <guiban10000@gmail.com>
2021-08-24 22:57:20 -0300

    Iniciando programa

--- Projeto 2D/Assets/Folders/BanzaFolder/PlayerMovement.cs
+++ Projeto 2D/Assets/Folders/BanzaFolder/PlayerMovement.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moviment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public KeyCode UpKey;
+   public KeyCode DownKey;
+   public KeyCode LeftKey;
+   public KeyCode RightKey;
+   public float Speed = 2.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+   private Rigidbody2D Rigidbody2DReference;
+
+
+
+   private void Awake()
+   {
+      if (UpKey == KeyCode.None)
+         UpKey = KeyCode.W;
+      if (DownKey == KeyCode.None)
+         DownKey = KeyCode.S;
+      if (LeftKey == KeyCode.None)
+         LeftKey = KeyCode.A;
+      if (RightKey == KeyCode.None)
+         RightKey = KeyCode.D;
+
+   }
+
 }
