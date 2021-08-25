732d61944839e630834a7edc23d9d984c77c2865
GuiBanzato <guiban10000@gmail.com>
2021-08-25 18:48:32 -0300

    arrumando

--- Projeto 2D/Assets/Folders/BanzaFolder/PlayerMovement.cs
+++ Projeto 2D/Assets/Folders/BanzaFolder/PlayerMovement.cs
@@ -10,22 +10,31 @@ public class PlayerMovement : MonoBehaviour
    public KeyCode RightKey;
    public float Speed = 2.0f;
 
-   
+   private Direction LastDirection;
    private Rigidbody2D Rigidbody2DReference;
-
-
-
+   
    private void Awake()
    {
       if (UpKey == KeyCode.None)
          UpKey = KeyCode.W;
+      
+      
       if (DownKey == KeyCode.None)
          DownKey = KeyCode.S;
+      
+      
       if (LeftKey == KeyCode.None)
          LeftKey = KeyCode.A;
+      
+      
       if (RightKey == KeyCode.None)
          RightKey = KeyCode.D;
 
+
+
+
+      Rigidbody2DReference = GetComponent<Rigidbody2D>();
+      
    }
 
 }
