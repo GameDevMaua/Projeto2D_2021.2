df60f6a7eb72d94bdb44d2512219e59d144010fd
ReiAkio <tanaka1557@gmail.com>
2021-08-28 02:19:21 -0300

    Sistema de "PauseMenu" adicionado
    "PauseMenu" em funcionamento, extensível para mais botões e jogável totalmente pelo teclado (ainda não foi desativado o uso do "mouse"(a discutir?). Adicionado também os comentários em c#Doc.

---
+++ Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/PauseMenu/PauseButtons.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseButtons : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject setSelectedButton;
+    
+    /// <summary>
+    /// Quando chamado, o metodo ira desativar o painel do pauseMenu e voltar o jogo ao seu devido tempo.
+    /// </summary>
+    public void resumeButton()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    /// <summary>
+    ///  Este Update() passa a funcionar quando o pauseMenu esta ativado. Detecta o botao escolhido e
+    /// o deixa selecionado para o funcionamento no teclado.
+    /// </summary>
+    private void Update()
+    {
+        if (GameObject.ReferenceEquals(EventSystem.current.currentSelectedGameObject, null))
+        {
+            EventSystem.current.SetSelectedGameObject(setSelectedButton);
+        }
+    }
+}
---
+++ Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] public GameObject pauseMenu;
+    public static bool isPaused;
+    
+    /// <summary>
+    /// Quando chamado, ira ativar o painel do menuPause, e pausar o estado do jogo
+    /// </summary>
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+    /// <summary>
+    /// Quando chamado, ira desativar o painel do menuPause, e despausar o estado do jogo.
+    /// </summary>
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    
+    /// <summary>
+    ///  Ao inicio, declara a variavel  isPaused para false.
+    /// </summary>
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+
+    /// <summary>
+    /// Update() responsável por checar quando o "player" pressionar a tecla "esc", ao ser
+    /// pressionado, ira checar se a variavel isPaused for verdadeira, se estiver ira chamar o metodo "Resume()"
+    /// e "setar" a variavel para false. Caso a mesma esteja falsa, chama-se o metodo "Pause()" e torna verdadeira
+    /// a variavel isPaused.
+    /// </summary>
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+                isPaused = false;
+            }
+            else if (isPaused == false)
+            {
+                Pause();
+                isPaused = true;
+            }
+        }
+    }
+}
