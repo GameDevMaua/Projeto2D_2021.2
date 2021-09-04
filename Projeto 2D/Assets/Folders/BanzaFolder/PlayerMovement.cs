c0a5d4bbfd7be24fc8101f1b24af895a44e836a5
GuiBanzato <guiban10000@gmail.com>
2021-09-04 18:45:32 -0300

    PlayerMovement

--- Projeto 2D/Assets/Folders/BanzaFolder/PlayerMovement.cs
+++ Projeto 2D/Assets/Folders/BanzaFolder/PlayerMovement.cs
@@ -9,31 +9,51 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode DownKey;
     public KeyCode LeftKey;
     public KeyCode RightKey;
-    public float Speed = 2.0f;
+    public float Speed = 2.5f;
 
     private Direction LastDirection;
     private Rigidbody2D Rigidbody2DReference;
 
+    private void Start()
+    {
+        Rigidbody2DReference = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
-        if (Input.GetKey(UpKey)) 
-            transform.Translate(Vector2.up * Speed);
-            
-        if (Input.GetKey(DownKey))
-            transform.Translate(Vector2.down * Speed);
-            
-        if (Input.GetKey(LeftKey))
-            transform.Translate(Vector2.left * Speed);
-            
-        if (Input.GetKey(RightKey)) 
-            transform.Translate(Vector2.right * Speed);
+        if (Input.GetKey(UpKey))
+        {
+            Rigidbody2DReference.velocity = new Vector2(Rigidbody2DReference.velocity.x, Speed);
+        }
 
+        else if (Input.GetKey(DownKey))
+        {
+            Rigidbody2DReference.velocity = new Vector2(Rigidbody2DReference.velocity.x, -Speed);
+        }
 
-        Rigidbody2DReference = GetComponent<Rigidbody2D>();
+        else
+        {
+            Rigidbody2DReference.velocity = new Vector2(Rigidbody2DReference.velocity.x, 0);
+        }
+
+        if (Input.GetKey(RightKey))
+        {
+            Rigidbody2DReference.velocity = new Vector2(Speed, Rigidbody2DReference.velocity.y);
+        }
+        
+        else if (Input.GetKey(LeftKey))
+        {
+            Rigidbody2DReference.velocity = new Vector2(-Speed, Rigidbody2DReference.velocity.y);
+        }
+
+        else
+        {
+            Rigidbody2DReference.velocity = new Vector2(0, Rigidbody2DReference.velocity.y);
+        }
             
+        
     }
-    
-    
-
 }
+
+    
 
