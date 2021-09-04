8baf88f49d56c60af4dd071946f7c4b191705a3e
ReiAkio <tanaka1557@gmail.com>
2021-09-04 17:02:25 -0300

    Sistema de PauseMenu aprimorado 1.2

--- Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/PauseMenu/PauseButtons.cs
+++ Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/PauseMenu/PauseButtons.cs
@@ -20,15 +20,20 @@ public class PauseButtons : MonoBehaviour
         Time.timeScale = 1;
         pM.isPaused = false;
     }
+
+    private void selectedButton()
+    {
+        if (GameObject.ReferenceEquals(EventSystem.current.currentSelectedGameObject, null))
+        {
+            EventSystem.current.SetSelectedGameObject(setSelectedButton);
+        }
+    }
     /// <summary>
     ///  Este Update() passa a funcionar quando o pauseMenu esta ativado. Detecta o botao escolhido e
     /// o deixa selecionado para o funcionamento no teclado.
     /// </summary>
     private void Update()
     {
-        if (GameObject.ReferenceEquals(EventSystem.current.currentSelectedGameObject, null))
-        {
-            EventSystem.current.SetSelectedGameObject(setSelectedButton);
-        }
+        selectedButton();
     }
 }
