c1201ec774dc2b30661df90a325fc253a9a4c4ad
raphazik4 <rapha.marchetti@gmail.com>
2021-10-04 19:48:14 -0300

    Sistema de chaves e portas Implementado
    - Implementado sistema inicial de chave
    - Implementado sistema inicial de portas (fechaduras)
        -> As fechaduras permitem a utilização de mais de uma chave para sua abertura
    - Sincronia com o sistema de alavanca (ativação e desativação de objetos)

---
+++ Projeto 2D/Assets/Folders/RaphaFolder/Scripts/LockScript.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour
+{
+    public List<GameObject> keys;
+    
+    private bool isOpened;
+    private GameObject playerRef;
+    
+    private void Awake()
+    {
+        if (keys.Count == 0) Debug.Log("Atenção, nenhuma chave atribuída à fechadura: " + gameObject);
+        playerRef = GameObject.FindWithTag("Player");
+        isOpened = false;
+    }
+
+    private void OnEnable()
+    {
+        if (isOpened) gameObject.SetActive(false);
+    }
+    
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.Equals(playerRef)) return;
+        if (keys.Any(VARIABLE => VARIABLE.GetComponent<KeyScript>().isCollected == false)) return;
+        isOpened = true;
+        gameObject.SetActive(false);
+    }
+}
