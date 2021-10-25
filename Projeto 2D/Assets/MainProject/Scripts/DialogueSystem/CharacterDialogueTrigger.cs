62a2a34c3ef65235acfb0c8298b35a58d57e5dcd
raphazik4 <rapha.marchetti@gmail.com>
2021-10-25 20:14:34 -0300

    Finalizada Versão inicial dos Pré-Fabs de diálogos
    - Finalizada versão inicial dos pré-fabs de diálogos
        . Diálogo de personagem (character dialogue):
            .. Diálogo que aparece sobre um personagem quando o player se aproxima e/ou sobre o player em si também.
        .Diálogo de Interação (estilo visual novel):
            .. Diálogo estilo visual novel no qual aparece a imagem do personagem que conversa com o player ao pressionar 'Z' por padrão.
        .Diálogo de cutscene:
            . Idêntico ao diálogo de interação, porém é iniciado sozinho quando o manager dele é ativado e leva para outra cena.

---
+++ Projeto 2D/Assets/MainProject/Scripts/DialogueSystem/CharacterDialogueTrigger.cs
@@ -0,0 +1,83 @@
+﻿
+
+using System;
+using UnityEngine;
+
+namespace MainProject.Scripts.DialogueSystem
+{
+    /// <summary>
+    /// O trigger do characterdialogue, ou seja, o dialogo é ativado quando o player se aproxima do gameobject e aparece o dialogo
+    /// </summary>
+    public class CharacterDialogueTrigger : MonoBehaviour
+    {
+        public GameObject entityCharacterDialogueGameObject;            // Game Object do characterDialogue da entidade do diálogo
+        public CharacterDialogueManager entityCharacterDialogueManager; // Referência do Script do DialogueManager
+        public bool aux;
+        public Dialogue dialogue;
+        
+        private bool playerInRange;
+
+        
+
+
+        /// <summary>
+        /// Funcao para comecar o diálogo
+        /// </summary>
+        public void TriggerDialogue()
+        {
+            FindObjectOfType<CharacterDialogueManager>().StartDialogue(dialogue);
+            entityCharacterDialogueManager.FunctionToDeactivateDialogue();
+        }
+    
+        /// <summary>
+        /// Quando o player estiver dentro da área, comeca o diálogo
+        /// </summary>
+        public void Update()
+        {
+    
+            if (playerInRange == true && aux == true) // se estiver dentro do playerInRange
+            {
+                if (entityCharacterDialogueManager.isDialogue == false)
+                {
+                    // Inicializa o diálogo, caso esse não esteja inicializado
+                    TriggerDialogue();
+                    aux = false;
+    
+                }
+    
+            }
+            
+            if (playerInRange == false)
+            {
+                aux = true;
+            }
+        }
+            
+        /// <summary>
+        /// Quando o player estiver dentro da área, inicia o dialogo
+        /// </summary>
+        /// <param name="other"></param>
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.attachedRigidbody)
+            {
+                playerInRange = true;
+                entityCharacterDialogueGameObject.SetActive(true);
+            }
+    
+        }
+        
+        /// <summary>
+        /// Quando o player estiver fora da área, termina o dialogo
+        /// </summary>
+        /// <param name="other"></param>
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.attachedRigidbody)
+            {
+                playerInRange = false;
+                entityCharacterDialogueManager.EndDialogue();
+            }
+        }
+    }
+}
