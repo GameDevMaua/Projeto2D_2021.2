5ca4f8a9978358cc0b66dadf85fd1e213479f5cb
ReiAkio <tanaka1557@gmail.com>
2021-10-25 16:29:01 -0300

    Implementado criação de multiplos wallDrawers

--- Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/MapSystem/MultiWallDrawer.cs
+++ Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/MapSystem/MultiWallDrawer.cs
@@ -8,5 +8,12 @@ public class MultiWallDrawer : MonoBehaviour
     public LineDrawer ld;
     
     public GameObject wallLine;
-    
+
+    public void createWallLine()
+    {
+        GameObject duplicate = Instantiate(wallLine.gameObject);
+        duplicate.transform.SetParent(GameObject.FindGameObjectWithTag("WallLiner").transform, false);
+        duplicate.GetComponent<LineDrawer>().enabled = true;
+    }
+
 }
