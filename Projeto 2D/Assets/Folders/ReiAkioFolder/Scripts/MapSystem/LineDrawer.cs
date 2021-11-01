8f0f716acbbb1493c01b307699dde77cd217acd5
ReiAkio <tanaka1557@gmail.com>
2021-11-01 12:13:28 -0300

    Implementado metodo para deletar ultimo filho criado

--- Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/MapSystem/LineDrawer.cs
+++ Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/MapSystem/LineDrawer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class LineDrawer : MonoBehaviour
@@ -36,8 +37,8 @@ public class LineDrawer : MonoBehaviour
             {
 
                 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                lr.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 3f));
-                lr.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 3f));
+                lr.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, -3f));
+                lr.SetPosition(1, new Vector3(mousePos.x, mousePos.y, -3f));
                 distance = (mousePos - startMousePos).magnitude;
                 distanceText.text = distance.ToString("F2") + " meters";
                 
@@ -68,5 +69,6 @@ public class LineDrawer : MonoBehaviour
         wallDrawer();
         
     }
+    
 
 }
--- Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/MapSystem/MultiWallDrawer.cs
+++ Projeto 2D/Assets/Folders/ReiAkioFolder/Scripts/MapSystem/MultiWallDrawer.cs
@@ -7,25 +7,42 @@ using UnityEngine.EventSystems;
 public class MultiWallDrawer : MonoBehaviour, IPointerClickHandler
 {
     public LineDrawer ld;
-    
+
     public GameObject wallLine;
+    public GameObject wallLineDrawer;
     public bool deleteCountAux = false;
 
     public void createWallLine()
     {
-        GameObject duplicate = Instantiate(wallLine.gameObject);
+        GameObject duplicate = Instantiate(wallLineDrawer.gameObject);
         duplicate.transform.SetParent(GameObject.FindGameObjectWithTag("WallLiner").transform, false);
         duplicate.GetComponent<LineDrawer>().enabled = true;
+        duplicate.transform.tag = "Clone";
         
     }
-
     public void OnPointerClick(PointerEventData eventData)
     {
+        
+        // Deletar ultimo filho criado
         if (eventData.button == PointerEventData.InputButton.Right)
         {
             deleteCountAux = true;
-            
+            deletingLastChild();
         }
+        
+        // Deletar gameObject clicado duas vezes
+        int i = eventData.clickCount;
+        if(i == 2 && ld.gameObject.CompareTag("Clone"))
+            Destroy(ld.gameObject);
     }
+
+    public void deletingLastChild()
+    {
+        int numChildren = wallLine.transform.childCount; 
+        Destroy(wallLine.transform.GetChild(numChildren-1).gameObject);
+        deleteCountAux = false;
+    }
+    
+    
 
 }
