b87b28229afe27e35fc54118ad4a13ea71029798
raphazik4 <rapha.marchetti@gmail.com>
2022-02-22 00:10:43 -0300

    Correção bug do savesystem
    - Feita a mesma coisa de antes

--- Projeto 2D/Assets/MainProject/Scripts/SaveSystemManager.cs
+++ Projeto 2D/Assets/MainProject/Scripts/SaveSystemManager.cs
@@ -2,21 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
+using FirstButton = MainProject.Scripts.FirstButton;
 
 
 public class SaveSystemManager : MonoBehaviour
 {
     [SerializeField] private string reachedScene;
     public GameObject[] levelPages;
-    public MainMenu mainMenu;
-    public MainProject.Scripts.FirstButton fb;
-    public List<GameObject> back = new List<GameObject>();
+    public GameObject[] back;
 
     private void Awake()
     {
-        
         levelPages[0].SetActive(false);
         levelPages[1].SetActive(false);
         levelPages[2].SetActive(false);
@@ -31,88 +30,87 @@ public class SaveSystemManager : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 0; i < levelPages.Length; i++)
-        {
-            var index1 = (levelPages[i].transform.childCount);
-            var index2 = (levelPages[i].transform.GetChild(index1-1).childCount);
-            var temp = levelPages[i].transform.GetChild(index1-1).GetChild(index2-1).gameObject;
-            back.Add(temp);
-        }
-        
         showLevelPage();
-        
     }
 
     private void showLevelPage()
     {
-        MainProject.Scripts.PlayerData reloadedData = MainProject.Scripts.SaveSystem.LoadData();
-        switch (char.GetNumericValue(reloadedData.savedScene[0]))
+        var reloadedData = MainProject.Scripts.SaveSystem.LoadData();
+        if (reloadedData == null)
+        {
+            levelPages[6].SetActive(true);
+            EventSystem.current.SetSelectedGameObject(back[6]);
+            return;
+        }
+        
+        var level = char.GetNumericValue(reloadedData.savedScene[0]);
+        switch (level)
         {
             case (0):
                 levelPages[0].SetActive(true);
-                fb.SelectFirstButton(back[0]);
+                EventSystem.current.SetSelectedGameObject(back[0]);
                 break;
             case (1):
                 levelPages[0].SetActive(true);
-                fb.SelectFirstButton(back[0]);
+                EventSystem.current.SetSelectedGameObject(back[0]);
                 break;
             case (2):
                 levelPages[1].SetActive(true);
-                fb.SelectFirstButton(back[1]);
+                EventSystem.current.SetSelectedGameObject(back[1]);
                 break;
             case (3):
                 levelPages[1].SetActive(true);
-                fb.SelectFirstButton(back[1]);
+                EventSystem.current.SetSelectedGameObject(back[1]);
                 break;
             case (4):
                 levelPages[2].SetActive(true);
-                fb.SelectFirstButton(back[2]);
+                EventSystem.current.SetSelectedGameObject(back[2]);
                 break;
             case (5):
                 levelPages[2].SetActive(true);
-                fb.SelectFirstButton(back[2]);
+                EventSystem.current.SetSelectedGameObject(back[2]);
                 break;
             case (6):
                 levelPages[3].SetActive(true);
-                fb.SelectFirstButton(back[3]);
+                EventSystem.current.SetSelectedGameObject(back[3]);
                 break;
             case (7):
                 levelPages[3].SetActive(true);
-                fb.SelectFirstButton(back[3]);
+                EventSystem.current.SetSelectedGameObject(back[3]);
                 break;
             case (8):
                 levelPages[4].SetActive(true);
-                fb.SelectFirstButton(back[4]);
+                EventSystem.current.SetSelectedGameObject(back[4]);
                 break;
             case (9):
                 levelPages[5].SetActive(true);
-                fb.SelectFirstButton(back[5]);
+                EventSystem.current.SetSelectedGameObject(back[5]);
                 break;
             default:
                 levelPages[6].SetActive(true);
-                fb.SelectFirstButton(back[6]);
+                EventSystem.current.SetSelectedGameObject(back[6]);
                 break;
         }
     }
 
-    private void correctLevelsShown()
-    {
-        MainProject.Scripts.PlayerData reloadedData = MainProject.Scripts.SaveSystem.LoadData();
-        for (int j = 0; j < levelPages.Length; j++)
-        {
-            if (reloadedData == null)
-                GameObject.Find("levePage Back");
-
-            else
-            {
-                if (char.GetNumericValue(reloadedData.savedScene[0]) >=
-                    int.Parse(levelPages[j].name.Substring(levelPages[j].name.Length - 1)))
-                {
-                    Debug.Log(levelPages[j] + "nome da cena");
-                    levelPages[j].SetActive(true);
-                }
-            }
-        }
-        
-    }
+    // private void correctLevelsShown()
+    // {
+    //     MainProject.Scripts.PlayerData reloadedData = MainProject.Scripts.SaveSystem.LoadData();
+    //     for (int j = 0; j < levelPages.Length; j++)
+    //     {
+    //         if (reloadedData == null)
+    //             GameObject.Find("levePage Back");
+    //
+    //         else
+    //         {
+    //             if (char.GetNumericValue(reloadedData.savedScene[0]) >=
+    //                 int.Parse(levelPages[j].name.Substring(levelPages[j].name.Length - 1)))
+    //             {
+    //                 Debug.Log(levelPages[j] + "nome da cena");
+    //                 levelPages[j].SetActive(true);
+    //             }
+    //         }
+    //     }
+    //     
+    // }
 }
